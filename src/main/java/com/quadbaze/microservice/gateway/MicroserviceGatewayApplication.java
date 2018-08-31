ac66521889ef42b623bca5c15a885acb4e3e1c2c
Olatunji Longe <olatunji.longe@gmail.com>
2018-08-31 00:13:58 -0400

    implemented feign / circuit-breaker

---
+++ src/main/java/com/quadbaze/microservice/gateway/MicroserviceGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.quadbaze.microservice.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceGatewayApplication.class, args);
+    }
+}
