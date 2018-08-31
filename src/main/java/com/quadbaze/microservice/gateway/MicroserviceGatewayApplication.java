37016730a24bf64e6c3a5dbbe9193ea9332d77c8
Olatunji Longe <olatunji.longe@gmail.com>
2018-08-31 00:35:10 -0400

    implemented zuul request routing

--- src/main/java/com/quadbaze/microservice/gateway/MicroserviceGatewayApplication.java
+++ src/main/java/com/quadbaze/microservice/gateway/MicroserviceGatewayApplication.java
@@ -2,7 +2,15 @@ package com.quadbaze.microservice.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableZuulProxy
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceGatewayApplication {
 
